8182806b9b80f2a6a6f512ddc68d2066fbb4afaf
Alexander <alexkorpol1964@gmail.com>
2023-04-04 16:17:34 +0300

    Last correct and proof of work (addition  Notiflix messages)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,6 @@
 // !=================================import libraries ==================================
 import throttle from 'lodash.throttle';
+import Notiflix from 'notiflix';
 // !=================  Block "Declare of constants & variables"==========================
 let data = {
   email:   "",
@@ -18,7 +19,8 @@ Form.addEventListener("submit", sendForm);
 // !===================== Block "Declare of functions" =============================
 
 function repairForLocalStorage() {
-const repairData = JSON.parse(localStorage.getItem(KEY_DATA));
+  const repairData = JSON.parse(localStorage.getItem(KEY_DATA));
+
   if (repairData === null) {
     return;
   } else {
@@ -29,7 +31,6 @@ const repairData = JSON.parse(localStorage.getItem(KEY_DATA));
     }
 }
 function writeInputToLocalStorage(event) {
-  console.log("event.target.value:", event.target.value);
   data[event.target.name] = event.target.value;
   localStorage.setItem(KEY_DATA, JSON.stringify(data));
 };
@@ -37,18 +38,32 @@ function writeInputToLocalStorage(event) {
 function sendForm(event) {
   event.preventDefault();
 
-  if (data.email === "" || data.message === "")
-  { return };
-
+  if (data.email === "")
+  { return Notiflix.Notify.failure(`❌ Please fill the field Email`);
+  };
+  if (data.message === "") {
+    Notiflix.Notify.success(`✅ Оnly your Email has been received`);
+  } else {
+    Notiflix.Notify.success(`✅ Your request has been received successfully`)
+  };
+  // Clear data of localStorage
   localStorage.removeItem(KEY_DATA);
-  event.currentTarget.reset();
 
+  // Clear date of form
+  event.currentTarget.reset();
+  inputEmail.value = "";
+  inputTextArea.textContent = "";
+
+  console.log("data of feedback-form SENT on back-end!!!", data);
+
+  // Clear data of script for NEXT USE!!!
   data = {
      email:   "",
      message: "",
   };
+  console.log("data of feedback-form CLEAR AFTER SENT on back-end!!!", data);
 
-  console.log("data after clear of feedback-form",data);
+
 };
 
 // !================================= EOF =========================================
